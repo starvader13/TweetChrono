e404d44a9261aff4d574ed45d4e3e7ac36247df7
starvader13 <ashutoshgupta1311@gmail.com>
2023-09-05 19:08:48 +0530

    Add Flash Message
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
     def index
-        
+        # flash.now[:notice] = 'Logged in successfully'
+        # flash[:alert] = 'Invalid email-id or password'
     end
 end
---
+++ docs/notes.md
@@ -0,0 +1,4 @@
+## ERB syntax
+
+With the equal to `<%= %>` it prints the value as returned
+But if the equal to is removed the ruby code is just executed
