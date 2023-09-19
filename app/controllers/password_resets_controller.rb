994f51628432829c73b4d2c96615447321eb8f9e
starvader13 <ashutoshgupta1311@gmail.com>
2023-09-19 18:30:18 +0530

    Complete reset password functionality using token mailer
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,4 +13,26 @@ class PasswordResetsController < ApplicationController
         redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset that password"
 
     end
+
+    def edit
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Your token has expired. PLease try again"
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your Password was reset successfully. PLease sign in"
+        else
+            redirect_to action: :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
+
 end
